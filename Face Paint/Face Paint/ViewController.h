b34d8c05eef2ce2f5167a1ba4c5c2bfe1e67dfb5
bhtran <bhtran00@gmail.com>
2016-05-31 14:19:43 -0400

    Initial add of VC

---
+++ Face Paint/Face Paint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Face Paint
+//
+//  Created by Bao Tran on 5/31/16.
+//  Copyright © 2016 Bao Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Face Paint/Face Paint/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Face Paint
+//
+//  Created by Bao Tran on 5/31/16.
+//  Copyright © 2016 Bao Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
