daa62168f2bdc061a8af90f6eff3e7e5d988ec0e
bhtran <bhtran00@gmail.com>
2016-07-06 14:43:24 -0400

    gradient layer implemented

--- Face Paint/Face Paint/UserInterfaceViewController.m
+++ Face Paint/Face Paint/UserInterfaceViewController.m
@@ -41,16 +41,26 @@
     UIButton *selectAndPlayButton = [UIButton buttonWithType:(UIButtonTypeCustom)];
     UIButton *recordAndSaveButton = [UIButton buttonWithType:(UIButtonTypeCustom)];
     UIButton *mergeAndSaveButton = [UIButton buttonWithType:(UIButtonTypeCustom)];
-    
+    UIButton *overlayButton = [UIButton buttonWithType:(UIButtonTypeCustom)];
+    UIButton *subtitleButton = [UIButton buttonWithType:(UIButtonTypeCustom)];
+    UIButton *animationButton = [UIButton buttonWithType:(UIButtonTypeCustom)];
+    UIButton *tiltButton = [UIButton buttonWithType:(UIButtonTypeCustom)];
+    UIButton *borderButton = [UIButton buttonWithType:(UIButtonTypeCustom)];
+
     //    UIView *self.view = self.view;
     
     [self.view addSubview:selectAndPlayButton];
     [self.view addSubview:recordAndSaveButton];
     [self.view addSubview:mergeAndSaveButton];
-    
+    [self.view addSubview:overlayButton];
+    [self.view addSubview:subtitleButton];
+    [self.view addSubview:animationButton];
+    [self.view addSubview:tiltButton];
+    [self.view addSubview:borderButton];
+
     [recordAndSaveButton mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.view.mas_top).with.offset(20);
-        make.left.equalTo(self.view.mas_left).with.offset(30);
+        make.left.equalTo(self.view.mas_left).with.offset(20);
         make.right.equalTo(selectAndPlayButton.mas_left).with.offset(-20);
         make.bottom.equalTo(self.view.mas_bottom).with.offset(-20);
     }];
@@ -59,24 +69,39 @@
         make.top.equalTo(self.view.mas_top).with.offset(20);
         make.left.equalTo(recordAndSaveButton.mas_right).with.offset(20);
         make.right.equalTo(mergeAndSaveButton.mas_left).with.offset(-20);
-        make.bottom.equalTo(self.view.mas_bottom).with.offset(-20);
+        make.bottom.equalTo(recordAndSaveButton.mas_bottom);
     }];
     
     [mergeAndSaveButton mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.view.mas_top).with.offset(20);
         make.right.equalTo(self.view.mas_right).with.offset(-20);
-        make.bottom.equalTo(self.view.mas_bottom).with.offset(-20);
+        make.bottom.equalTo(recordAndSaveButton.mas_bottom);
         
     }];
     
+//    [overlayButton mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.top.equalTo()
+//    }];
+//    
     recordAndSaveButton.layer.borderWidth = 2;
-    recordAndSaveButton.layer.borderColor = [[UIColor blackColor] CGColor];
-    
     mergeAndSaveButton.layer.borderWidth = 2;
-    mergeAndSaveButton.layer.borderColor = [[UIColor blackColor] CGColor];
-    
     selectAndPlayButton.layer.borderWidth = 2;
+
+    recordAndSaveButton.layer.borderColor = [[UIColor blueColor] CGColor];
+    mergeAndSaveButton.layer.borderColor = [[UIColor redColor] CGColor];
     selectAndPlayButton.layer.borderColor = [[UIColor blackColor] CGColor];
+    
+    // Gradient Background
+    UIColor *topColor = [UIColor colorWithRed:222.0f/255.0f green:222.0f/255.0f blue:222.0f/255.0f alpha:.85];
+    UIColor *bottomColor = [UIColor colorWithRed:255.0f/255.0f green:251.0f/255.0f blue:232.0f/255.0f alpha:.20];
+    
+    CAGradientLayer *theViewGradient = [CAGradientLayer layer];
+    theViewGradient.colors = [NSArray arrayWithObjects:(id)topColor.CGColor, (id)bottomColor.CGColor, nil];
+    theViewGradient.frame = self.view.bounds;
+    
+    [self.view.layer insertSublayer:theViewGradient atIndex:0];
+
+    self.view.backgroundColor = [UIColor colorWithRed:84.0f/255.0f green:84.0f/255.0f blue:84.0f/255.0f alpha:1];
 
 }
 
